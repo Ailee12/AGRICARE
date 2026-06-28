cb00351da03478ca4216b449a47b581e1d220bb9
Ailee12 <106934283+Ailee12@users.noreply.github.com>
2026-06-28 08:39:57 +0100

    Configure database settings for production
    Added database configuration using dj_database_url.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,10 +1,19 @@
 import os
 from .base import *
 import environ
+import dj_database_url
 
 env = environ.Env()
 
 DEBUG = False
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
+
 ALLOWED_HOSTS = [
     'agrocare-production-61a9d.up.railway.app',  # Your current active domain
     '.up.railway.app',                            # Wildcard for any fallback Railway domain
